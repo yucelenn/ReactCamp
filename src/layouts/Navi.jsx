8895f855db0116eadb7d3ca771d8489da25ed021
yucelenn <yucelenn@hotmail.com>
2024-06-03 22:55:08 +0300

    day 11

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,38 +1,30 @@
-import React from 'react'
-import {
-    DropdownMenu,
-    DropdownItem,
-    MenuMenu,
-    MenuItem,
-    Button,
-    Dropdown,
-    Menu,
-    Container,
-} from 'semantic-ui-react'
+import React from "react";
+import CartSummary from "./CartSummary";
+import { Button, Container, Dropdown, Menu } from "semantic-ui-react";
 
 export default function Navi() {
-    return (
-        <div>
-            <Menu inverted fixed>
-                <Container>
-                    <MenuItem name='home' />
-                    <MenuItem name='messages' />
+  return (
+    <div>
+      <Menu inverted fixed>
+        <Container>
+          <Menu.Item name="home" />
+          <Menu.Item name="messages" />
 
-                    <Menu.Menu position='right'>
-                        <Dropdown item text='Language'>
-                            <Dropdown.Menu>
-                                <Dropdown.Item>English</Dropdown.Item>
-                                <Dropdown.Item>Russian</Dropdown.Item>
-                                <Dropdown.Item>Spanish</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
+          <Menu.Menu position="right">
+            <Dropdown item text="Language">
+              <Dropdown.Menu>
+                <Dropdown.Item>English</Dropdown.Item>
+                <Dropdown.Item>Russian</Dropdown.Item>
+                <Dropdown.Item>Spanish</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
 
-                        <Menu.Item>
-                            <Button primary>Sign Up</Button>
-                        </Menu.Item>
-                    </Menu.Menu>
-                </Container>
-            </Menu>
-        </div>
-    )
+            <Menu.Item>
+              <Button primary>Sign Up</Button>
+            </Menu.Item>
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    </div>
+  );
 }
