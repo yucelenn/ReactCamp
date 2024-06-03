ac504e26968667094fdec2e597507fa771f0b7ef
yucelenn <yucelenn@hotmail.com>
2024-06-03 22:44:26 +0300

    ilk

---
+++ src/layouts/Navi.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+    DropdownMenu,
+    DropdownItem,
+    MenuMenu,
+    MenuItem,
+    Button,
+    Dropdown,
+    Menu,
+    Container,
+} from 'semantic-ui-react'
+
+export default function Navi() {
+    return (
+        <div>
+            <Menu inverted fixed>
+                <Container>
+                    <MenuItem name='home' />
+                    <MenuItem name='messages' />
+
+                    <Menu.Menu position='right'>
+                        <Dropdown item text='Language'>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>English</Dropdown.Item>
+                                <Dropdown.Item>Russian</Dropdown.Item>
+                                <Dropdown.Item>Spanish</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+
+                        <Menu.Item>
+                            <Button primary>Sign Up</Button>
+                        </Menu.Item>
+                    </Menu.Menu>
+                </Container>
+            </Menu>
+        </div>
+    )
+}
